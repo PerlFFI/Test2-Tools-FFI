84087d5e0e649c1dad36f68b7ad997ab93d4c228
Graham Ollis <plicease@cpan.org>
2019-02-12 21:43:41 -0500

    add ok

--- share/include/t2t/simple.h
+++ share/include/t2t/simple.h
@@ -10,5 +10,6 @@ int t2t_simple_fail(const char *, const char *, const char *, int, const char *)
 #define diag(message) t2t_simple_diag(message, "c", __FILE__, __LINE__, __func__)
 #define pass(message) t2t_simple_pass(message, "c", __FILE__, __LINE__, __func__)
 #define fail(message) t2t_simple_fail(message, "c", __FILE__, __LINE__, __func__)
+#define ok(expression, message) expression ? t2t_simple_pass(message, "c", __FILE__, __LINE__, __func__) : t2t_simple_fail(message, "c", __FILE__, __LINE__, __func__)
 
 #endif
